67b37e2a236184c38f3cadc267fae31b6b391cc3
Senna46 <29295263+Senna46@users.noreply.github.com>
2021-10-12 16:02:34 +0900

    feat: build

---
+++ build/gcc.debug/CMakeFiles/validator-keys.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/user/github.com/ripple/validator-keys-tool/src/ValidatorKeys.cpp" "/home/user/github.com/ripple/validator-keys-tool/build/gcc.debug/CMakeFiles/validator-keys.dir/src/ValidatorKeys.cpp.o"
+  "/home/user/github.com/ripple/validator-keys-tool/src/ValidatorKeysTool.cpp" "/home/user/github.com/ripple/validator-keys-tool/build/gcc.debug/CMakeFiles/validator-keys.dir/src/ValidatorKeysTool.cpp.o"
+  "/home/user/github.com/ripple/validator-keys-tool/src/test/ValidatorKeysTool_test.cpp" "/home/user/github.com/ripple/validator-keys-tool/build/gcc.debug/CMakeFiles/validator-keys.dir/src/test/ValidatorKeysTool_test.cpp.o"
+  "/home/user/github.com/ripple/validator-keys-tool/src/test/ValidatorKeys_test.cpp" "/home/user/github.com/ripple/validator-keys-tool/build/gcc.debug/CMakeFiles/validator-keys.dir/src/test/ValidatorKeys_test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ASIO_DISABLE_HANDLER_TYPE_REQUIREMENTS"
+  "BOOST_ASIO_USE_TS_EXECUTOR_AS_DEFAULT"
+  "BOOST_BEAST_ALLOW_DEPRECATED"
+  "BOOST_COROUTINES_NO_DEPRECATION_WARNING"
+  "BOOST_FILESYSTEM_DEPRECATED"
+  "HAS_UNCAUGHT_EXCEPTIONS=1"
+  "OPENSSL_NO_SSL2"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../src"
+  "../../.nih_c/unix_makefiles/GNU_9.4.0/Release/rippled_src-src/src"
+  "../../.nih_c/unix_makefiles/GNU_9.4.0/Release/rippled_src-src/src/ripple"
+  "../../.nih_c/unix_makefiles/GNU_9.4.0/Release/rippled_src-src/src/beast/extras"
+  "/home/user/boost_1_75_0"
+  "../../.nih_c/unix_makefiles/GNU_9.4.0/Release/src/hh_date_src/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/user/github.com/ripple/validator-keys-tool/.nih_c/unix_makefiles/GNU_9.4.0/Release/rippled_src-build/CMakeFiles/xrpl_core.dir/DependInfo.cmake"
+  "/home/user/github.com/ripple/validator-keys-tool/.nih_c/unix_makefiles/GNU_9.4.0/Release/rippled_src-build/CMakeFiles/secp256k1.dir/DependInfo.cmake"
+  "/home/user/github.com/ripple/validator-keys-tool/.nih_c/unix_makefiles/GNU_9.4.0/Release/rippled_src-build/CMakeFiles/ed25519-donna.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/gcc.debug/CMakeFiles/validator-keys.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/validator-keys.dir/src/ValidatorKeys.cpp.o"
+  "CMakeFiles/validator-keys.dir/src/ValidatorKeysTool.cpp.o"
+  "CMakeFiles/validator-keys.dir/src/test/ValidatorKeys_test.cpp.o"
+  "CMakeFiles/validator-keys.dir/src/test/ValidatorKeysTool_test.cpp.o"
+  "validator-keys.pdb"
+  "validator-keys"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/validator-keys.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
